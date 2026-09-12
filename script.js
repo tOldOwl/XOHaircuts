0f3bd8f2e06d55a1357bc42553e233ae3e5d19ad
Owl of your Dreamz <tpaintedowl@gmail.com>
2026-09-11 23:40:06 -0700

    Modify wording for directory button in script.js
    Updated dynamic wording for the directory button.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const viewImages = {
 
 // 3. Set up dynamic wording configurations
 const viewTexts = {
-  'directory-btn': 'Just north of the mall,<br> across the ravine',
+  'directory-btn': 'Just north of the mall,<br> across the ravine.<br>Bouquet / Espuella',
   'schedule-btn': 'Walk-In<br>Schedule',
   'aboutus-btn': 'Greeting! My name is Clint. <br>I opened XO Haircuts in<br> 2015. After many years of<br> success cutting peoples hair.'
 };
